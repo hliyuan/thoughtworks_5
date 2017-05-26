42f70f2e722b32564860cf93427b743889b00dbf
liyuan Huang <liyuan Huang>
2017-05-26 11:33:14 +0800

    commit

--- main/main.js
+++ main/main.js
@@ -22,21 +22,21 @@ class Sequence {
     return array;
   }
 
-  minimum() {
+  function minimum(){
     // Write your code here
     array.sort();
     return array[0];
   }
 
   // Write your code here
-  maxnum(){
+  function maxnum(){
 	array.sort();
     return array[array.length-1];
   }
-  size(){
+  function size(){
   	return array.length;
   }
-  avg(){
+  function avg(){
   	var sum = 0;
   	for (var i = 0; i < array.length; i++) {
   		sum += array[i]
