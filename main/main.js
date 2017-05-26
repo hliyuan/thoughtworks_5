6756b2db1920319c49f0da8d606e3ef3a38a909a
liyuan Huang <liyuan Huang>
2017-05-26 11:30:06 +0800

    commit

--- main/main.js
+++ main/main.js
@@ -3,16 +3,44 @@ module.exports = function main() {
     console.log(`o) 最小值 = ${sequence.minimum()}
 // Write your code here
 `);
+    console.log(`o) 最大值 = ${sequence.maxnum()}
+// Write your code here
+`);
+    console.log(`o) 元素数量 = ${sequence.size()}
+// Write your code here
+`);
+    console.log(`o) 平均值 = ${sequence.avg()}
+// Write your code here
+`);
 };
 
 class Sequence {
   constructor(input) {
     // Write your code here
+    var array = new Array();
+    array.push(input);
+    return array;
   }
 
   minimum() {
     // Write your code here
+    array.sort();
+    return array[0];
   }
 
   // Write your code here
+  maxnum(){
+	array.sort();
+    return array[array.length-1];
+  }
+  size(){
+  	return array.length;
+  }
+  avg(){
+  	var sum = 0;
+  	for (var i = 0; i < array.length; i++) {
+  		sum += array[i]
+  	}
+  	return (sum/array.length).toFixed(2);
+  }
 }
